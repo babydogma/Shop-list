2ba43d89631c4b203ed96f5c6bc6328ec0c6439a
babydogma <123qqwes@mail.ru>
2026-07-09 14:49:58 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,60 @@
-const CACHE = "shopping-list-v7";
-const FILES = [
-    "./",
-    "./index.html",
-    "./archive.html",
-    "./style.css",
-    "./script.js",
-    "./manifest.json"
+const CACHE_NAME = 'shopping-list-pwa-v1';
+
+const APP_ASSETS = [
+  './',
+  './index.html',
+  './archive.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
-self.addEventListener("install", event => {
-    self.skipWaiting();
-    event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_ASSETS))
+  );
+
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys()
-            .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
-            .then(() => self.clients.claim())
-    );
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
-    if (event.request.mode === "navigate") {
-        event.respondWith(fetch(event.request).catch(() => caches.match("./index.html")));
-        return;
-    }
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
 
-    event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) {
+        return cached;
+      }
+
+      return fetch(event.request)
+        .then(response => {
+          const copy = response.clone();
+
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, copy);
+          });
+
+          return response;
+        })
+        .catch(() => caches.match('./index.html'));
+    })
+  );
 });
