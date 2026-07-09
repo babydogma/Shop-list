a07235b8f71e29742ce6612d32b593d4b8425588
babydogma <123qqwes@mail.ru>
2026-07-09 10:07:46 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,31 @@
-const CACHE="shopping-list-v2";
-const FILES=[
-"./",
-"./index.html",
-"./archive.html",
-"./style.css",
-"./script.js",
-"./manifest.json"
+const CACHE = "shopping-list-v4";
+const FILES = [
+    "./",
+    "./index.html",
+    "./archive.html",
+    "./style.css",
+    "./script.js",
+    "./manifest.json"
 ];
 
-self.addEventListener("install",e=>{
-e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+self.addEventListener("install", event => {
+    self.skipWaiting();
+    event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
 });
 
-self.addEventListener("fetch",e=>{
-e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys()
+            .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+            .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener("fetch", event => {
+    if (event.request.mode === "navigate") {
+        event.respondWith(fetch(event.request).catch(() => caches.match("./index.html")));
+        return;
+    }
+
+    event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
